2c6a52aec6b45727eb7fdc103ba2ff953b6860fb
Romlonix <descarga7@hotmail.com>
2020-10-30 11:17:08 -0600

    v1.0 menú estable

---
+++ js/my-js.js
@@ -0,0 +1,14 @@
+document.getElementById("navbar-toggler").onclick = navbarToggler;
+
+function navbarToggler() {
+  document.getElementById("nav-dropdown").classList.toggle("display-f");
+
+  if (document.getElementById("navbar-toggler").classList.contains("fa-bars") == true) {
+    document.getElementById("navbar-toggler").classList.remove("fa-bars");
+    document.getElementById("navbar-toggler").classList.add("fa-times");
+  } else {
+    document.getElementById("navbar-toggler").classList.remove("fa-times");
+    document.getElementById("navbar-toggler").classList.add("fa-bars");
+  }
+}
+
