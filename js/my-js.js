ecf4a2c7a703aac754a83769326346ec2ff6f7c5
Romlonix <descarga7@hotmail.com>
2020-11-01 20:43:18 -0600

    Se termino el diseño responsivo

--- js/my-js.js
+++ js/my-js.js
@@ -2,19 +2,9 @@ document.getElementById("navbar-toggler").onclick = function () {
 
   document.getElementById("nav-dropdown").classList.toggle("visibility-v");
 
-  document.getElementById("header-menu").classList.toggle("b-shadow");
-
   document.getElementById("navbar-toggler").classList.toggle("fa-bars");
 
   document.getElementById("navbar-toggler").classList.toggle("fa-times");
-
-  // if (document.getElementById("navbar-toggler").classList.contains("fa-bars") == true) {
-  //   document.getElementById("navbar-toggler").classList.remove("fa-bars");
-  //   document.getElementById("navbar-toggler").classList.add("fa-times");
-  // } else {
-  //   document.getElementById("navbar-toggler").classList.remove("fa-times");
-  //   document.getElementById("navbar-toggler").classList.add("fa-bars");
-  // }
 
 };
 
