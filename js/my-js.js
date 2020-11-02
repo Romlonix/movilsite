aca50fa138ff47ec423752b0781c4e58f3930b8f
Romlonix <descarga7@hotmail.com>
2020-11-01 18:56:13 -0600

    Se mejoro el Js con .toggle

--- js/my-js.js
+++ js/my-js.js
@@ -4,13 +4,17 @@ document.getElementById("navbar-toggler").onclick = function () {
 
   document.getElementById("header-menu").classList.toggle("b-shadow");
 
-  if (document.getElementById("navbar-toggler").classList.contains("fa-bars") == true) {
-    document.getElementById("navbar-toggler").classList.remove("fa-bars");
-    document.getElementById("navbar-toggler").classList.add("fa-times");
-  } else {
-    document.getElementById("navbar-toggler").classList.remove("fa-times");
-    document.getElementById("navbar-toggler").classList.add("fa-bars");
-  }
+  document.getElementById("navbar-toggler").classList.toggle("fa-bars");
+
+  document.getElementById("navbar-toggler").classList.toggle("fa-times");
+
+  // if (document.getElementById("navbar-toggler").classList.contains("fa-bars") == true) {
+  //   document.getElementById("navbar-toggler").classList.remove("fa-bars");
+  //   document.getElementById("navbar-toggler").classList.add("fa-times");
+  // } else {
+  //   document.getElementById("navbar-toggler").classList.remove("fa-times");
+  //   document.getElementById("navbar-toggler").classList.add("fa-bars");
+  // }
 
 };
 
